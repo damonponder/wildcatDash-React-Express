8fdde9a1a88e43dedaae4c7aa2f43fdffda716be
dgr8ninja <dr4602@gmail.com>
2019-11-15 15:27:24 -0600

    remove client submodule and add project

---
+++ client/src/components/header.jsx
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+
+export default class Header extends Component {
+    render(){
+        return (
+        <ul>
+            <li>Home</li>
+            <li>Dashboard</li>
+            <li>WildCat U</li>
+            <li>About Us</li>
+            <li>Login</li>
+        </ul>
+        )
+
+    }
+}
